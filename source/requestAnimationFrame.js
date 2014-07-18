8391f44c3dc3c2b34d1f9663903efb18bae025dc
Patrik Wilhelmsson <patrik.wilhelmsson@minmb.se>
2014-07-18 10:51:00 +0200

    blacklisting devices with broken RAF implementation.

--- source/requestAnimationFrame.js
+++ source/requestAnimationFrame.js
@@ -1,34 +1,32 @@
-/**
- * Request Animation Frame Polyfill.
- * @author Tino Zijdel
- * @author Paul Irish
- * @see https://gist.github.com/paulirish/1579671
- */
-;(function() {
-
-  var lastTime = 0;
-  var vendors = ['ms', 'moz', 'webkit', 'o'];
-
-  for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-    window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-    window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
-  }
-
-  if (!window.requestAnimationFrame) {
-    window.requestAnimationFrame = function(callback, element) {
-      var currTime = new Date().getTime();
-      var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-      var id = window.setTimeout(function() { callback(currTime + timeToCall); },
-        timeToCall);
-      lastTime = currTime + timeToCall;
-      return id;
-    };
-  }
-
-  if (!window.cancelAnimationFrame) {
-    window.cancelAnimationFrame = function(id) {
-      clearTimeout(id);
-    };
-  }
-
-}());
+// requestAnimationFrame implementation as a custom function to allow blacklisting
+// devices with broken implementation. Currently needs timer-based fallbacks for iOS 6.x for
+// code running inside <iframe> elements.
+// Uses polyfills based on http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+(function() {
+ 
+    var blacklisted   = /iP(ad|hone|od).*OS 6/.test(window.navigator.userAgent),
+        lastTime      = 0,
+        nativeRequest = window.requestAnimationFrame || null,
+        nativeCancel  = window.cancelAnimationFrame  || null;
+ 
+    ['webkit', 'moz'].forEach(function(prefix) {
+        nativeRequest = nativeRequest || window[prefix+'RequestAnimationFrame'] || null;
+        nativeCancel  = nativeCancel  || window[prefix+'CancelAnimationFrame']  || window[prefix+'CancelRequestAnimationFrame'] || null;
+    });
+ 
+    function polyfillRequest(callback, element) {
+        var currTime = new Date().getTime();
+        var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+        var id = window.setTimeout(function() { callback(currTime + timeToCall); }, timeToCall);
+        lastTime = currTime + timeToCall;
+        return id;
+    }
+ 
+    function polyfillCancel(id) {
+        clearTimeout(id);
+    }
+ 
+    this.nextFrame   = (!blacklisted && nativeRequest !== null) ? nativeRequest : polyfillRequest;
+    this.cancelFrame = (!blacklisted && nativeCancel  !== null) ? nativeCancel  : polyfillCancel;
+ 
+})();
